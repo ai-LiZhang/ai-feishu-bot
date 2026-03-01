c3500a492b9d8a9c2ab57d6fe1f01076e68ac37e
ai-LiZhang <zhangli0773@163.com>
2026-03-01 17:15:25 +0800

    Update ai_report.py

--- ai_report.py
+++ ai_report.py
@@ -2,24 +2,25 @@ import os
 import requests
 import datetime
 
-WEBHOOK = os.getenv("https://open.feishu.cn/open-apis/bot/v2/hook/893db3b5-3857-4b75-826a-9fa8ea4f986b")
-
 print("Script started")
+
+WEBHOOK = os.getenv("https://open.feishu.cn/open-apis/bot/v2/hook/893db3b5-3857-4b75-826a-9fa8ea4f986b")
 
 today = datetime.date.today()
 
-report = f"""
-AI产业投资情报日报
+message = f"""
+AI系统测试成功
 
 日期：{today}
 
-AI系统测试成功
+如果你看到这段文字，
+说明 GitHub Actions 已经在运行新的代码。
 """
 
 data = {
     "msg_type": "text",
     "content": {
-        "text": report
+        "text": message
     }
 }
 
